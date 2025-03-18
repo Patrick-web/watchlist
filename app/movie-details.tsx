42edc71a320e186e7e959ac0739a1d6b8fd1dcea
Just Patrick <pntxall100@gmai.com>
2025-03-19 02:59:14 +0300

    Extract film header and info components

--- app/movie-details.tsx
+++ app/movie-details.tsx
@@ -1,5 +1,5 @@
 import Box from "@/components/reusables/Box";
-import { POSTER_RATIO } from "@/constants/dimensions.constant";
+import { POSTER_RATIO, sHeight } from "@/constants/dimensions.constant";
 import React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Image, ImageBackground } from "expo-image";
@@ -11,11 +11,13 @@ import ThemedButton from "@/components/reusables/ThemedButton";
 import ThemedActivityIndicator from "@/components/reusables/ThemedActivityIndicator";
 import ThemedText from "@/components/reusables/ThemedText";
 import ThemedListItem from "@/components/reusables/ThemedListItem";
-import { addMovieToWatchList } from "@/valitio.store";
+import { addMovieToWatchList, isInWatchList } from "@/valitio.store";
 import { LinearGradient } from "expo-linear-gradient";
 import { useTheme, useThemeMode } from "@/hooks/useTheme.hook";
 import { Platform } from "react-native";
-import { ScreenRoot } from "@/components/PlatformScreenRoot";
+import he from "he";
+import FilmInfo from "@/components/FilmInfo";
+import FilmHeader from "@/components/FilmHeader";
 
 const POSTER_HEIGHT = 250;
 const POSTER_WIDTH = POSTER_HEIGHT / POSTER_RATIO;
@@ -57,89 +59,24 @@ const Film = () => {
     router.replace("/(tabs)/watchlist");
   }
   return (
-    <ScreenRoot>
+    <Box
+      height={sHeight - insets.bottom - insets.top}
+      justify="space-between"
+      color={"background"}
+      gap={10}
+    >
       <Box>
-        <ImageBackground
-          source={movie.poster}
-          blurRadius={100}
-          style={{
-            width: "100%",
-          }}
-        >
-          {Platform.OS !== "ios" && (
-            <ThemedButton
-              icon={{ name: "arrow-left", color: "white" }}
-              type="surface"
-              position="absolute"
-              color={"rgba(0,0,0,0.2)"}
-              top={insets.top}
-              left={10}
-              onPress={router.back}
-              zIndex={40}
-            />
-          )}
-          <Box
-            block
-            align="center"
-            color={"rgba(0,0,0,0.3)"}
-            pt={Platform.OS === "ios" ? 20 : insets.top}
-          >
-            <Image
-              source={movie.poster}
-              style={{
-                width: POSTER_WIDTH,
-                height: POSTER_HEIGHT,
-                borderRadius: POSTER_WIDTH / 2,
-              }}
-            />
-
-            <LinearGradient
-              colors={[
-                themeMode === "dark"
-                  ? "rgba(32,32,32,0)"
-                  : "rgba(255,255,255,0)",
-                theme.background,
-              ]}
-              style={{ width: "100%" }}
-            >
-              <Box px={20} py={10}>
-                <ThemedText size={"xxl"} align="center" fontWeight="bold">
-                  {movie.title}
-                </ThemedText>
-              </Box>
-            </LinearGradient>
-          </Box>
-        </ImageBackground>
+        <FilmHeader film={movie} />
         {isLoading ? (
           <ThemedActivityIndicator />
         ) : (
-          filmInfo !== undefined && (
-            <Box block>
-              <ThemedListItem
-                label="Duration"
-                value={filmInfo.duration}
-                varaint="horizontal"
-              />
-              <ThemedListItem
-                label="Released"
-                value={filmInfo.releaseDate}
-                varaint="horizontal"
-              />
-              <ThemedListItem
-                label="Rating"
-                value={filmInfo.rating}
-                varaint="horizontal"
-              />
-              <ThemedListItem label="Genre" value={filmInfo.genre} />
-              <ThemedListItem label="Cast" value={filmInfo.casts.join(", ")} />
-            </Box>
-          )
+          filmInfo !== undefined && <FilmInfo filmInfo={filmInfo} />
         )}
       </Box>
       <Box gap={20} direction="row" px={20}>
         <ThemedButton
-          label={"Add to Watchlist"}
-          type="surface"
+          label={isInWatchList(movie.url) ? "Watching Later" : "Watch Later"}
+          type={isInWatchList(movie.url) ? "primary" : "surface"}
           direction="column"
           size="sm"
           py={10}
@@ -147,7 +84,7 @@ const Film = () => {
           flex={1}
         />
       </Box>
-    </ScreenRoot>
+    </Box>
   );
 };
 
