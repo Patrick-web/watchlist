09501caa63f9182a69a2d71a47e5e65912601835
Just Patrick <pntxall100@gmai.com>
2025-03-24 10:27:34 +0300

    Remove unused hslDarken utility function

--- utils/ui.utils.ts
+++ utils/ui.utils.ts
@@ -1,11 +1,6 @@
 export function generateRandomPastelColor() {
-	const hue = Math.floor(Math.random() * 360);
-	const pastel = `hsl(${hue}, 100%, 90%)`;
-	const contrast = `hsl(${(hue + 180) % 360}, 100%, 50%)`;
-	return { pastel, contrast };
-}
-
-export function hslDarken(color: string, amount: number) {
-	const [hue, sat, light] = color.match(/\d+/g)!.map(Number);
-	return `hsl(${hue}, ${sat}%, ${light - amount}%)`;
+  const hue = Math.floor(Math.random() * 360);
+  const pastel = `hsl(${hue}, 100%, 90%)`;
+  const contrast = `hsl(${(hue + 180) % 360}, 100%, 50%)`;
+  return { pastel, contrast };
 }
